63e861dd60076cb8aaafb9722deb81f321a71018
debianordie <110401470+debianordie@users.noreply.github.com>
2022-08-01 22:09:03 +0000

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-. ~/.bash_aliases
+. ~/.dod
 
 package update
 package upgrade
@@ -7,7 +7,7 @@ package install sway
 package install telegram-desktop keepassx zim virt-manager terminator vim git geany-plugin-treebrowser
 package install webext-https-everywhere webext-privacy-badger webext-keepassxc-browser webext-ublock-origin-firefox
 
+cp -a ${BASEDIR}/imports.sh ~/.config/dod/10_sway.sh
+
 mkdir -p ~/.config/sway
 cp -a ${BASEDIR}/sway.conf ~/.config/sway/config
-
-. ~/.bash_aliases
