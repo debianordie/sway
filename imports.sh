3399b095937b23d482f54aa79ba298cc98bf8e29
debianordie <110401470+debianordie@users.noreply.github.com>
2022-08-01 22:06:57 +0000

    Create imports.sh

---
+++ imports.sh
@@ -0,0 +1,8 @@
+export MOZ_ENABLE_WAYLAND=1
+export XDG_SESSION_TYPE=wayland 
+export GDK_BACKEND=wayland
+export QT_QPA_PLATFORM=wayland
+
+if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
+  exec sway
+fi
