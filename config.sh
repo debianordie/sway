51832651e53bac94c7443285ac78925b912d98ef
debianordie <110401470+debianordie@users.noreply.github.com>
2022-08-01 22:06:26 +0000

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,26 +1,11 @@
 #!/bin/sh
-
-BASEDIR=$(dirname "$0")
-
-export DEBIAN_FRONTEND=noninteractive
-
-alias package='sudo apt-get --quiet --quiet --yes'
-alias service='sudo systemctl --quiet'
+. ~/.bash_aliases
 
 package update
 package upgrade
 package install sway
 package install telegram-desktop keepassx zim virt-manager terminator vim git geany-plugin-treebrowser
 package install webext-https-everywhere webext-privacy-badger webext-keepassxc-browser webext-ublock-origin-firefox
-
-echo 'export MOZ_ENABLE_WAYLAND=1
-export XDG_SESSION_TYPE=wayland 
-export GDK_BACKEND=wayland
-export QT_QPA_PLATFORM=wayland
-# automatically login into sway
-if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
-  exec sway
-fi' > ~/.bash_aliases
 
 mkdir -p ~/.config/sway
 cp -a ${BASEDIR}/sway.conf ~/.config/sway/config
