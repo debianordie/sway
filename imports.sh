9d4af9a64fdc1b243e3d167ede810b62cc17df26
debianordie <110401470+debianordie@users.noreply.github.com>
2022-08-02 11:58:46 +0000

    Update imports.sh

--- imports.sh
+++ imports.sh
@@ -3,6 +3,6 @@ export XDG_SESSION_TYPE=wayland
 export GDK_BACKEND=wayland
 export QT_QPA_PLATFORM=wayland
 
-if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
-  exec sway
-fi
+#if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
+  pgrep --exact sway >/dev/null || exec sway
+#fi
