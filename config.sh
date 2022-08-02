5925edf964e389d54f621e0f471ef0aea6d1bd4c
debianordie <110401470+debianordie@users.noreply.github.com>
2022-08-02 00:54:21 +0000

    Update config.sh

--- config.sh
+++ config.sh
@@ -4,7 +4,6 @@
 ensure_dod_module 'desktop'
 
 package install sway
-cp -a ${BASEDIR}/imports.sh ~/.config/dod/10_sway.sh
 
 mkdir -p ~/.config/sway
 cp -a ${BASEDIR}/sway.conf ~/.config/sway/config
