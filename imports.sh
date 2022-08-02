4f402032cd724d1514fac127dc7d789140e2f531
debianordie <110401470+debianordie@users.noreply.github.com>
2022-08-02 12:06:45 +0000

    Update imports.sh

--- imports.sh
+++ imports.sh
@@ -2,7 +2,3 @@ export MOZ_ENABLE_WAYLAND=1
 export XDG_SESSION_TYPE=wayland 
 export GDK_BACKEND=wayland
 export QT_QPA_PLATFORM=wayland
-
-#if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
-  pgrep --exact sway >/dev/null || exec sway
-#fi
